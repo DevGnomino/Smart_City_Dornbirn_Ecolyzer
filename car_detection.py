6fe9de2655aca3c9219ff69cb15f1546a6e1e849
Mert <martinschneider22@gmx.net>
2023-01-16 08:37:33 +0100

    opencv car detection

---
+++ car_detection.py
@@ -0,0 +1,17 @@
+# Import libraries
+#from PIL import Image
+#import cv2
+#import numpy as np
+#import requests
+
+#image = Image.open(requests.get('https://a57.foxnews.com/media.foxbusiness.com/BrightCove/854081161001/201805/2879/931/524/854081161001_5782482890001_5782477388001-vs.jpg', stream=True).raw)
+#image = image.resize((450,250))
+#image_arr = np.array(image)
+
+#carDetector = cv2.CascadeClassifier('cas4.xml')
+
+#results = carDetector.detectMultiScale(gray_img, scaleFactor=1.05,minNeighbors=5,minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+#print(results)
+
+print("Hello")

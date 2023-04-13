4ad9b2c39e54345951f67a9134bf1eca7a3c3b24
DevGnomino <damianopezze@gmail.com>
2023-04-13 13:46:28 +0200

    map included

--- ecolyzer/src/App.jsx
+++ ecolyzer/src/App.jsx
@@ -5,11 +5,11 @@ import Map from './components/Map';
 function App() {
   return (
     <div className="">
-      <div className="w-screen flex justify-center items-center border-solid border-black border-4 box-border">
-        <div className="text-4xl font-bold p-5">ECOLYZER</div>
+      <div className="h-10 w-screen flex justify-center items-center border-solid border-black border-4 box-border">
+        <div className="text-4xl font-bold p-2">ECOLYZER</div>
       </div>
-      <div className="h-[100px] w-screen-6 m-3 flex border-solid rounded-lg border-black border-4 box-border">
-        <div className="h-full w-2/3 bg-slate-400"></div>
+      <div className="h-85 w-screen-6 m-3 flex border-solid rounded-lg border-black border-4 box-border">
+        <div className="h-full w-2/3 bg-slate-400"><Map/></div>
         <div className="h-full w-1/3 bg-slate-800"><SlideOver/></div>
         
       </div>

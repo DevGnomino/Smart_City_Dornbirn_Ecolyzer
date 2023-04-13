b9a3dc79191a648793a2b6073ddf936b7f5eb1b3
DevGnomino <damianopezze@gmail.com>
2023-04-13 13:42:54 +0200

    basic site design

--- ecolyzer/src/App.jsx
+++ ecolyzer/src/App.jsx
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import SlideOver from './components/SlideOver';
 import Map from './components/Map';
 
 function App() {
   return (
-    <div className="w- shadow-md rounded-full">
-    <h1 className="text-3xl font-mono center">
-      Hello world!
-    </h1>
-      <SlideOver/>
-    </div>  
-
-    );
+    <div className="">
+      <div className="w-screen flex justify-center items-center border-solid border-black border-4 box-border">
+        <div className="text-4xl font-bold p-5">ECOLYZER</div>
+      </div>
+      <div className="h-[100px] w-screen-6 m-3 flex border-solid rounded-lg border-black border-4 box-border">
+        <div className="h-full w-2/3 bg-slate-400"></div>
+        <div className="h-full w-1/3 bg-slate-800"><SlideOver/></div>
+        
+      </div>
+    </div>
+  );
 }
 
 export default App;

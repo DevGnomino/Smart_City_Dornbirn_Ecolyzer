38e3f2f1a7f9267a2c6217430442dc0cd0232d43
Mertinus <martinschneider22@gmx.net>
2023-04-13 11:48:19 +0200

    map added but doesnt fully work

--- ecolyzer/src/components/Map.jsx
+++ ecolyzer/src/components/Map.jsx
@@ -1,25 +1,19 @@
 import React from 'react'
-//import { MapContainer } from 'https://cdn.esm.sh/react-leaflet/MapContainer';
-//import { TileLayer } from 'https://cdn.esm.sh/react-leaflet/TileLayer';
-//import { useMap } from 'https://cdn.esm.sh/react-leaflet/hooks';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 export default function Map() {
-  /*return (    
-    <div>
+  return (    
+    <div class="w-full h-full border-solid border-4 rounded-none">
         <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-        <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-        </Marker>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
         </MapContainer>
-  </div>
-  )*/
-  return (
-    <p>Hollo Map!</p>
-  )
+  </div>);
 }

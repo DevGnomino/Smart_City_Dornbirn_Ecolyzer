9da28df802863903e60719aa45fdb6fd5985c3a7
Mertinus <martinschneider22@gmx.net>
2023-04-13 11:16:02 +0200

    app structure established

--- ecolyzer/src/App.jsx
+++ ecolyzer/src/App.jsx
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Map from './Map.jsx';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>  
+    <div>
+      <div>
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>  
+      </div>
+      <Map></Map>
+    </div>
     );
 }
 

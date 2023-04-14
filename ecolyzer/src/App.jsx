a6e827822315a377b8c5b9014d24b869023bb7b9
Mertinus <martinschneider22@gmx.net>
2023-04-14 16:39:57 +0200

    merge commit

--- ecolyzer/src/App.jsx
+++ ecolyzer/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="h-screen w-screen box-border text-green-800 leading-none select-none">
       <div className="w-screen h-1/6 flex justify-evenly items-center box-border text-5xl">
-        <div className="w-3/4 h-fit flex justify-start flex items-center">
+        <div className="w-3/4 h-fit flex justify-start items-center">
         <div className="h-full font-josefin ml-5 p-2 pt-5 bg-green-800 rounded-2xl text-white text-6xl tracking-wide">ECOLYZER</div>
         </div>
         <div className="font-josefin w-1/4 flex justify-evenly ">

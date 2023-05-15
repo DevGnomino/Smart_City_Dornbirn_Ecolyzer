204b7a44286e4ceb3fe8f8572edd8369643fae37
DevGnomino <damianopezze@gmail.com>
2023-05-15 10:57:40 +0200

    minor responsiveness changes

--- ecolyzer/src/components/NavBar.jsx
+++ ecolyzer/src/components/NavBar.jsx
@@ -8,13 +8,13 @@ export default function NavBar() {
         <div>
             <div className="top-0 mb-1 w-screen h-32 flex justify-between items-center box-border text-5xl text-green-800 fixed bg-white z-10">
                 <div className="h-fit flex justify-start items-center">
-                    <div className="h-full font-josefin ml-5 p-2 pt-5 bg-green-800 rounded-2xl text-white text-5xl sm:text-6xl tracking-wide">ECOLYZER</div>
+                    <div className="h-full font-josefin ml-5 p-2 pt-5 bg-green-800 rounded-2xl text-white text-4xl sm:text-6xl tracking-wide">ECOLYZER</div>
                 </div>
-                <ul className="font-josefin h-fit mr-5 hidden md:flex justify-evenly">
+                <ul className="font-josefin h-fit mr-5 hidden lg:flex justify-evenly">
                     <li className="hover:scale-90 transition-all h-12 duration-500 m-4"><a href='/map'>Map</a></li>
                     <li className="hover:scale-90 transition-all h-12 duration-500 m-4"><a href='/about'>About</a></li>
                 </ul>
-                <div className="h-fit mr-5 md:hidden flex items-center hover:scale-90 transition-all duration-500 hover:cursor-pointer" onClick={() => { setShowMenu(!showMenu) }} >
+                <div className="h-fit mr-5 lg:hidden flex items-center hover:scale-90 transition-all duration-500 hover:cursor-pointer" onClick={() => { setShowMenu(!showMenu) }} >
                     {showMenu ? <BiX size={80} /> : <BiMenu size={80} />}
                 </div>
             </div>

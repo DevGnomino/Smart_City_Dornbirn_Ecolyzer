a9412d9e2cb934606246d7b56bef6b6483cf7355
Mertinus <martinschneider22@gmx.net>
2023-05-15 08:46:39 +0200

    resturctured the files into functions

--- car_detection.py
+++ car_detection.py
@@ -1,23 +1,45 @@
 # Import libraries
 from PIL import Image
 import numpy as np
-import cv2
+#import cv2
 import requests
+import time
+import influxdb_communication as influxWrapper
+
+def recogniseCars():
+    """carDetector = cv2.CascadeClassifier('cars.xml')
+
+    #img = cv2.imread('test.jpg')
+    image = Image.open(requests.get('https://a57.foxnews.com/media.foxbusiness.com/BrightCove/854081161001/201805/2879/931/524/854081161001_5782482890001_5782477388001-vs.jpg', stream=True).raw)
+    img = np.array(image)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #faces = carDetector.detectMultiScale(gray, 1.3, 5)
+    carCounter = 0
+    busCounter = 3
+    bikeCounter = 1
+    faces = carDetector.detectMultiScale(gray, scaleFactor=1.05,minNeighbors=5,minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        carCounter+=1
+
+    print(len(faces))
+    cv2.imshow('img', img)
+    cv2.waitKey(0)"""
+
+    carCounter = 11
+    busCounter = 3
+    bikeCounter = 1
+
+    return {"car": carCounter, "bus": busCounter, "bike": bikeCounter }
 
 
-carDetector = cv2.CascadeClassifier('cars.xml')
-
-#img = cv2.imread('test.jpg')
-image = Image.open(requests.get('https://a57.foxnews.com/media.foxbusiness.com/BrightCove/854081161001/201805/2879/931/524/854081161001_5782482890001_5782477388001-vs.jpg', stream=True).raw)
-img = np.array(image)
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#faces = carDetector.detectMultiScale(gray, 1.3, 5)
-faces = carDetector.detectMultiScale(gray, scaleFactor=1.05,minNeighbors=5,minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-print(len(faces))
-cv2.imshow('img', img)
-cv2.waitKey(0)
-
+if __name__ == "__main__":
+    while True:
+        cvResult = recogniseCars()
+        #influxWrapper.openInfluxConnection()
+        latLong = "47.502069,9.747105"
+        #influxWrapper.sendDataToInflux(latLong, cvResult["car"], cvResult["bus"], cvResult["bike"])
+        print(latLong, cvResult)
+        time.sleep(1)
+    
--- influxdb_communication.py
+++ influxdb_communication.py
@@ -7,20 +7,22 @@ org = "htldor"
 token = "W5W6CaN_pYQg9BOEMJRQRYwV17fzJo-vUaJDhBZ20o_cmlr0Xeo5RjvrTZpOHpmyY9D8ayqO-sF3nBpiR6Fh9A=="
 url="http://10.115.3.38:8086"
 
-client = influxdb_client.InfluxDBClient(
-   url=url,
-   token=token,
-   org=org
-)
+def openInfluxConnection():
+   global client 
+   client = influxdb_client.InfluxDBClient(
+      url=url,
+      token=token,
+      org=org
+   )
 
-write_api = client.write_api(write_options=SYNCHRONOUS)
+   global write_api
+   write_api = client.write_api(write_options=SYNCHRONOUS)
 
-#currTime = datetime.now()
-
-latLon = "47.502069,9.747105"
-allPoints = list()
-allPoints.append(influxdb_client.Point("my_measurement").tag("location", latLon).field("cars", 11))
-allPoints.append(influxdb_client.Point("my_measurement").tag("location", latLon).field("busses", 3))
-allPoints.append(influxdb_client.Point("my_measurement").tag("location", latLon).field("bike", 2))
-for point in allPoints:
-    write_api.write(bucket=bucket, org=org, record=point)
+def sendDataToInflux(latLong, nCars, nBusses, nBikes):
+   print(latLong, nCars, nBusses, nBikes)
+   allPoints = list()
+   allPoints.append(influxdb_client.Point("my_measurement").tag("location", latLong).field("cars", nCars))
+   allPoints.append(influxdb_client.Point("my_measurement").tag("location", latLong).field("busses", nBusses))
+   allPoints.append(influxdb_client.Point("my_measurement").tag("location", latLong).field("bike", nBikes))
+   for point in allPoints:
+      write_api.write(bucket=bucket, org=org, record=point)

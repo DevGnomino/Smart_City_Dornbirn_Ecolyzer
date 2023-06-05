45db047d7f1a87801128bd3ad42642580e0f1df6
Mertinus <martinschneider22@gmx.net>
2023-06-05 11:25:03 +0200

    influx data is shown on the map

--- ecolyzer/src/InfluxTest.jsx
+++ ecolyzer/src/InfluxTest.jsx
@@ -1,41 +1,55 @@
 import { InfluxDB } from "@influxdata/influxdb-client";
+import { useState, useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
 const bucket = "ecolyzer";
 const org = "htldor";
-const token = "8fkPIHpovQtjqafqg8HBadj5nA3_JTFg80vQ3UYMuxcH9BtS52OgIVvAGc68w1CjtDU8avJ8oWTPe8MRtlmgow==";
+const token = "L0t7zDPuCLXMkgJC1PSIRaO1ZB1VhiSIKnO-lnludXGtEhdN3WsvQLmy5aaHBdeVKAK6STrxLupuEcEHBKHbIw==";
 //Token 1:  8fkPIHpovQtjqafqg8HBadj5nA3_JTFg80vQ3UYMuxcH9BtS52OgIVvAGc68w1CjtDU8avJ8oWTPe8MRtlmgow==
 //Token 2:  W5W6CaN_pYQg9BOEMJRQRYwV17fzJo-vUaJDhBZ20o_cmlr0Xeo5RjvrTZpOHpmyY9D8ayqO-sF3nBpiR6Fh9A==
 const url = "http://10.115.3.38:8086"; 
     //Von zu Hause mit Tunnel: http://localhost:8086
     //In der Schule: http://10.115.3.38:8086/ 
 
-const query = 'from(bucket: "' + bucket + '") |> range(start: -30d)';
+const query = 'from(bucket: "' + bucket + '") |> range(start: -12h) |> tail(n: 30) |> movingAverage(n: 30)'; //5min
 
-export default async function GetInfluxData(setVehicleData) {
-    console.log("Get Data Function called!");
+export default function InfluxMarker(props) {
+    const [influxData, setInfluxData] = useState([{location:[47.421169, 9.747031]}])
+
     try {
-        const queryApi = new InfluxDB({url: url, token: token}).getQueryApi(org);
+        useEffect(() => {  
+            console.log("Get Data Function called!");
 
-        //Holt die Werte aus Influx, spaltet die Zeilen und danach die einzelnen Felder auf:
-        const result = await queryApi.queryRaw(query);
-        const resLines = result.split('\n');
-        resLines.pop();
-        resLines.pop();
-        //console.log(resLines);
-        let valueArray = [];
-        let lineCount = 0;
-        resLines.forEach(row => {
-            if (lineCount >= 4) {
-                //console.log(row);
-                let values = row.split(',');
-                //console.log(values[6] + " " + values[7] + " " + values[9])
-                valueArray.push({time: values[5], location: values[9], name: values[7], count: values[6]});
-            }
-            lineCount++;
-        });
+            const queryApi = new InfluxDB({url: url, token: token}).getQueryApi(org);
+
+            //Holt die Werte aus Influx, spaltet die Zeilen und danach die einzelnen Felder auf:
+            queryApi.queryRaw(query).then((result) => {
+                //console.log(result);        
+                const resLines = result.split('\n');
+                
+                let valueArray = [];
+                resLines.forEach(row => {
+                    if (row.length >= 2 && row[0] == ',' && row[1] == ',') {
+                        let values = row.split(',');
+                        //console.log(row);
+                        //console.log(values[6] + " " + values[7] + " " + values[9])
+                        //valueArray.push({time: values[5], location: values[9], name: values[7], count: values[6]});
+                        let lat = values[9].replace('"', '');
+                        let long = values[10].replace('"', '');
+                        valueArray.push({time: values[5], location: [lat,long], name: values[7], count: values[6]});
+                    }
+                });
+                console.log(valueArray);
+                setInfluxData(valueArray);
+            });
+        }, [])
+        
+        //console.log(valueArray);
         
         //Sortiert die Werte damit der aktuellste Wert oben ist:
-        for (let i = 0; i < valueArray.length; i++) {
+        /*for (let i = 0; i < valueArray.length; i++) {
             for (let j = 0; j < valueArray.length; j++) {
                 if (valueArray[i].time > valueArray[j].time) {
                     let tempValue = valueArray[i];
@@ -48,14 +62,24 @@ export default async function GetInfluxData(setVehicleData) {
                     valueArray[j] = tempValue;
                 }
             }
-        }
+        }*/
 
-        console.log(valueArray[0]);
-        console.log(valueArray[1]);
-        console.log(valueArray[2]);
-
-        setVehicleData({car: valueArray[0], bus: valueArray[1], bike: valueArray[2], location: valueArray[0].location});
-        //return {car: valueArray[0], bus: valueArray[1], bike: valueArray[2], location: valueArray[0].location};        
+        //setVehicleData(valueArray);
+        //return {car: valueArray[0], bus: valueArray[1], bike: valueArray[2], location: valueArray[0].location}; 
+        
+        
+        return (            
+            <Marker position={influxData[0].location} icon={props.DefaultIcon}>
+                <Popup>
+                    <p className="font-bold">Folgende Daten sind hier vorhanden:</p>
+                    <p>Autos: {influxData[1].count}<br></br>
+                       Busse: {influxData[0].count}<br></br>
+                       Zweiräder: {influxData[4].count}<br></br>
+                       Temperatur: {influxData[3].count}<br></br>
+                       Luftfeuchtigkeit: {influxData[2].count}</p>
+                </Popup>
+            </Marker>
+        ); 
     }
     catch(ex) {
         console.log("Fetch error: " + ex);
--- ecolyzer/src/components/MapComponent.jsx
+++ ecolyzer/src/components/MapComponent.jsx
@@ -8,17 +8,17 @@ import light_orange from './../icons/light_orange.png';
 import yellow from './../icons/yellow.png';
 import green from './../icons/green.png';
 import yellow_green from './../icons/yellow_green.png';
-import GetInfluxData from './../InfluxTest';
+import InfluxMarker from './../InfluxTest';
 
 
 export default function MapComponent() {
     const [icon, setIcon] = useState(red);
-    const [vehicleData, setVehicleData] = useState("Vehicle data not defined");
+    //const [vehicleData, setVehicleData] = useState("Vehicle data not defined");
 
-    const mapPosition = [47.421169, 9.747031];
+    const dornbirnCenter = [47.421169, 9.747031]; //47.421169, 9.747031
 
     useEffect(() => {    // Update the document title using the browser API    
-        GetInfluxData(setVehicleData);  
+        //GetInfluxData(setVehicleData);  
     }, []); 
 
     const DefaultIcon = L.icon({
@@ -28,23 +28,17 @@ export default function MapComponent() {
         popupAnchor: [0, 0]
     });
 
-    console.log(vehicleData);
+    //console.log(vehicleData);
 
     return (
         <div className="w-screen h-5/6 flex mt-32 p-5 pt-0 text-green-800 overflow-hidden fixed">
             <div id="map" className="w-full h-auto overflow-hidden border-solid border-4 border-green-800 rounded-2xl bg-blue box-border">
-                <MapContainer center={mapPosition} zoom={14} zoomControl={false} scrollWheelZoom={true} style={{ height: '100vh', width: '100wh' }}>
+                <MapContainer center={dornbirnCenter} zoom={14} zoomControl={false} scrollWheelZoom={true} style={{ height: '100vh', width: '100wh' }}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={mapPosition} icon={DefaultIcon}>
-                        <Popup>
-                            <p className="font-bold">Hier sind aktuell 5 Autos.</p>
-                            <p>Das ist wahrlich wunderbar.</p>
-                            <p>Wiedersehen!</p>
-                        </Popup>
-                    </Marker>
+                    <InfluxMarker DefaultIcon={DefaultIcon}></InfluxMarker>
                 </MapContainer>
             </div>
         </div>
